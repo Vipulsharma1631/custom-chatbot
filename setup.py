9cfd375c0ae065d3e05d2266c476133d8dd7269a
Vipulsharma1631 <Vipulsharma162002@Gmail.com>
2025-09-02 13:53:08 +0530

    frontend ready

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'custom-Chatbot',
+    version= '0.0.0',
+    author= 'vs',
+    author_email= 'vipulsharma.1631@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
